3f2d3f8e0eeab56e221d4cc5e806d4e7def94c76
Kinga Janiak <135730166+KingaJaniak@users.noreply.github.com>
2023-12-21 21:01:30 +0100

    Revert "add basic comunication with API"

--- src/js/fetch-from-api/fetch-book.js
+++
@@ -1,6 +0,0 @@
-import axios from "axios"
-
-export const fetchBookById = async bookId => {
-    const response = await axios.get(`https://books-backend.p.goit.global/books/${bookId}`);
-    return response;
-}
--- src/js/fetch-from-api/fetch-category.js
+++
@@ -1,6 +0,0 @@
-import axios from "axios"
-
-export const fetchCategory = async categoryName => {
-    const response = await axios.get(`https://books-backend.p.goit.global/books/category?category=${categoryName}`);
-    return response;
-}
