82fc6278158c60a38daa4fa03a31a95782bd8492
Kinga Janiak <janiak.kinga96@gmail.com>
2023-12-27 14:32:39 +0100

    menu-change

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,15 +1,28 @@
-document.addEventListener('DOMContentLoaded', function () {
+(() => {
   const mobileMenu = document.getElementById('mobileMenu');
   const toggleBtn = document.getElementById('toggleBtn');
-  const closeMenuBtn = document.getElementById('closeMenuBtn');
+  const closeBtn = document.getElementById('closeBtn');
 
-  toggleBtn.addEventListener('click', function () {
-    mobileMenu.classList.toggle('header-menu-open');
+  const toggleMenu = () => {
     const isMenuOpen = mobileMenu.classList.contains('header-menu-open');
-    toggleBtn.setAttribute('aria-expanded', String(isMenuOpen));
-  });
-  closeMenuBtn.addEventListener('click', function () {
+    mobileMenu.classList.toggle('header-menu-open');
+    toggleBtn.setAttribute('aria-expanded', !isMenuOpen);
+
+    if (isMenuOpen) {
+      toggleBtn.innerHTML =
+        '<svg width="28px" height="28px"><use href="//src/images/icons.svg#icon-align-left"></use></svg>';
+    } else {
+      toggleBtn.innerHTML =
+        '<svg width="28px" height="28px"><use href="/src/images/icons.svg#icon-x-close"></use></svg>';
+    }
+  };
+
+  toggleBtn.addEventListener('click', toggleMenu);
+  closeBtn.addEventListener('click', toggleMenu);
+
+  window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
+    if (!e.matches) return;
     mobileMenu.classList.remove('header-menu-open');
-    toggleBtn.setAttribute('aria-expanded', 'false');
+    toggleBtn.setAttribute('aria-expanded', false);
   });
-});
+})();
