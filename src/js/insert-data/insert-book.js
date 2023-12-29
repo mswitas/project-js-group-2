979b13acc7f1ad20b831f851fc425a4c664da250
mswitas <mswitas@gmail.com>
2023-12-29 21:36:00 +0100

    delete unnecessary files

--- src/js/insert-data/insert-book.js
+++ src/js/insert-data/insert-book.js
@@ -1,10 +1,11 @@
 import { fetchBookById } from "../fetch-from-api/fetch-book"
 import { errorHandle } from "./error-handle";
 
-export const insertBook = bookId => {
+export const insertBook = (bookId, selector) => {
     fetchBookById(bookId)
         .then(response => {
             console.log(response.data);
+            
         })
         .catch(error => {
             errorHandle(error);
