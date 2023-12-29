d7ba9898f77a3f9d6f382afbd608927cf35e672e
mswitas <mswitas@gmail.com>
2023-12-29 18:14:31 +0100

    add category-list link and events

--- src/js/insert-data/insert-category-list.js
+++ src/js/insert-data/insert-category-list.js
@@ -1,5 +1,6 @@
 import { fetchFromApi } from "../fetch-from-api/fetch-from-api"
 import { errorHandle } from "./error-handle";
+import { insertCategory } from "./insert-category";
 
 export const insertCategoryList = () => {
     fetchFromApi('category-list')
@@ -9,10 +10,16 @@ export const insertCategoryList = () => {
                 categoryList.push(value.list_name);
             });
             console.log(categoryList);
+            categoryList.sort();
             const elementList = document.querySelector('.bookshelf-categories-list');
-            categoryList.map(category => {
-                const listItem = `<li class="categories-list">${category}</li>`;
+            categoryList.map((category, index) => {
+                const listItem = `<li class="categories-list"><a href="#" id="category-${index}">${category}</a></li>`;
                 elementList.insertAdjacentHTML('beforeend', listItem);
+                const link = document.querySelector(`#category-${index}`);
+                link.addEventListener('click', (event) => {
+                    event.preventDefault();
+                    insertCategory(category);
+                });
             });
         })
         .catch(error => {
