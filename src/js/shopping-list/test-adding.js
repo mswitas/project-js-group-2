234922835c8fa464e003065a0b06516abfd1158f
ToporekDaniel <danieltoporek0@gmail.com>
2024-01-01 23:57:06 +0100

    usuwanie działa

--- src/js/shopping-list/test-adding.js
+++ src/js/shopping-list/test-adding.js
@@ -5,6 +5,7 @@ import { fetchBookById } from '../fetch-from-api/fetch-book';
 import { removeBookIdFromShoppingList } from './remove-book-id-from-shopping-list';
 
 const removeBtn = document.querySelectorAll('SL-remove');
+const listRemove = document.querySelector('#SL-list');
 
 // addBookIdToShoppingList('643282b1e85766588626a0dc');
 // addBookIdToShoppingList('643282b1e85766588626a0b6');
@@ -23,15 +24,24 @@ if (getShoppingList().length > 0) {
 
 getShoppingList().forEach(book => {
   fetchBookById(book).then(response => {
-    // Log fetched data
     console.log(response.data);
     createBook(response.data);
   });
 });
 
-removeBtn.forEach(btn => {
-  console.log(btn.value);
-  btn.addEventListener('click', function (event) {
-    removeBookIdFromShoppingList(this.value);
-  });
+listRemove.addEventListener('click', function (event) {
+  if (event.target.classList.contains('SL-remove')) {
+    console.log(event.target.value);
+    removeBookIdFromShoppingList(event.target.value);
+    location.reload();
+  }
+  // const shoppingListUl = document.querySelector('#SL-list');
+  // shoppingListUl.innerHTML = '';
+
+  // getShoppingList().forEach(book => {
+  //   fetchBookById(book).then(response => {
+  //     console.log(response.data);
+  //     createBook(response.data);
+  //   });
+  // });
 });
