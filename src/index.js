94f2bee971b2c149e9767f22272abb215c937e38
mswitas <mswitas@gmail.com>
2024-01-03 13:25:28 +0100

    fix index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import { insertCategoryList } from './js/insert-data/insert-category-list';
 import { insertTopBooks } from './js/insert-data/insert-top-books';
-import { insertCategory } from './js/insert-data/insert-category';
-import { insertBook } from './js/insert-data/insert-book';
 import { getTopBookCovers } from './js/insert-data/insert-top-book-covers';
 import './css/scss/main.scss';
 import './js/mobile-menu';
 import './js/theme-switcher';
 import './js/login-modal';
-import { getTest } from './js/insert-data/insert-test';
+
 
 insertCategoryList();
 insertTopBooks();
