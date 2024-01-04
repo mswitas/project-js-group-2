f12a2bb5f77b5d671dc78cd609d1a2540802098b
mswitas <43390604+mswitas@users.noreply.github.com>
2024-01-04 20:22:53 +0000

    Deploying to gh-pages from @ mswitas/project-js-group-2@d4b3b8bebc846c235bb44c7fe04944fa71565e8d 🚀

--- shopping.af39308d.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector('.theme-switch input[type="checkbox"]'),t=localStorage.getItem("theme");t&&(document.documentElement.setAttribute("data-theme",t),"dark"===t&&(e.checked=!0)),e.addEventListener("change",function(e){e.target.checked?(document.documentElement.setAttribute("data-theme","dark"),localStorage.setItem("theme","dark")):(document.documentElement.setAttribute("data-theme","light"),localStorage.setItem("theme","light"))},!1);
-//# sourceMappingURL=shopping.af39308d.js.map
