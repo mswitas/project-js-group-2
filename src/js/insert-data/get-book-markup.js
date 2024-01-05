9df489fc41437fab0d541be23b0ab9e1b80b2dd3
mswitas <mswitas@gmail.com>
2024-01-05 13:21:16 +0100

    add cover container

--- src/js/insert-data/get-book-markup.js
+++ src/js/insert-data/get-book-markup.js
@@ -1,17 +1,17 @@
 export const getBookMarkup = (coverImg, title, author) => {
     const markup = `
-       
-            <img
-                src="${coverImg}"
-                alt="Book cover"
-                class="book_cover"
-            />
-            <div class="book_cover_box_overlay">
-                <p class="book_cover_text_overlay">quick view</p>
+            <div class="book_cover_container">
+                <img
+                    src="${coverImg}"
+                    alt="Book cover"
+                    class="book_cover"
+                />
+                <div class="book_cover_box_overlay">
+                    <p class="book_cover_text_overlay">quick view</p>
+                </div>
             </div>
             <h3 class="book_cover_book_title">${title}</>
-            <h4 class="book_cover_book_subtitle">${author}</h4>
-        
+            <h4 class="book_cover_book_subtitle">${author}</h4>  
     `;
     return markup;
 }
