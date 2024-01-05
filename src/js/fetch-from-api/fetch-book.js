cd7fc924d1236a59520e79998d82880aaca5da1e
DurkaMateusz <mosca888@gmail.com>
2024-01-05 21:05:41 +0100

    loader

--- src/js/fetch-from-api/fetch-book.js
+++ src/js/fetch-from-api/fetch-book.js
@@ -1,6 +1,8 @@
 import axios from "axios"
+import { loadingInfo } from '../fetch-from-api/loader';
 
 export const fetchBookById = async bookId => {
+    loadingInfo();
     const response = await axios.get(`https://books-backend.p.goit.global/books/${bookId}`);
     return response;
 }
